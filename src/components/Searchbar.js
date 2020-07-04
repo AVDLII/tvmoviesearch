2398be3806808bce64370f9993b5bd7bb3f6413a
Agrielio V. De Lazo II <avdelazo221b@gmail.com>
2020-07-04 13:43:55 +0800

    added searchbar component

---
+++ src/components/Searchbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Searchbar() {
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" placeholder="Search" className="searchbox"/>
+        </section>
+    )
+}
+
+export default Searchbar
