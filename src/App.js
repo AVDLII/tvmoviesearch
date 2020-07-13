2a666f8ba14d5ee7fbf82b61609293071d2e7ef5
Agrielio V. De Lazo II <avdelazo221b@gmail.com>
2020-07-13 18:44:16 +0800

    added credits link

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
 import React, {useState} from 'react';
 import Searchbar from './components/Searchbar'
+import ResultingSearch from './components/ResultingSearch'
+import Info from './components/Info'
 import axios from 'axios'
 
-
 function App() {
-  const [state, setState] = useState({
-    searchQ: "",
-    selectedObject: {}
-  });
-  const url = "https://api.themoviedb.org/3/search/movie?";
-  const apikey = process.env.REACT_APP_TMDB_API_KEY;
+  const [searchTerm, setSearchTerm] = useState ({
+    search: "",
+    searchResults: [],
+    selectedResult: {}
+  })
+  const baseurl = "https://api.themoviedb.org/3/search/movie?api_key=";
+  const ak = process.env.REACT_APP_TMDB_API;
+  const popupurl = "https://api.themoviedb.org/3/movie/";
 
   const userSearch = (e) => {
-    if(e.key === "Enter") {
-      axios(url+"api_key="+apikey+"&query="+state.searchQ+"&page=1&include_adult=false").then(({data}) => {
-        console.log(data);
-        let result = data.Searchbar;
+    if (e.key === "Enter") {
+      console.log(searchTerm.search)
+      axios(baseurl+ak+"&query="+searchTerm.search).then(({ data }) => {
+        let searchResults = data.results;
 
-        setState(prevState => {
-          return { ...prevState, results: result}
+        setSearchTerm(prevState => {
+          return { ...prevState, searchResults: searchResults }
         })
-      });
+        console.log(searchTerm.searchResults);
+      })
     }
+  } 
+
+  const popUpInfo = id => {
+    axios(popupurl+id+"?api_key="+ak+"&append_to_response=credits").then(({ data }) => {
+      let searchResults = data;
+      console.log(searchResults)
+      setSearchTerm(prevState => {
+        return { ...prevState, selectedResult: searchResults }
+      })
+    })
   }
 
+  const exitPopUp = () => {
+    setSearchTerm(prevState => {
+      return { ...prevState, selectedResult: {} }
+    })
+  }
 
-  const input = (e) => {
-    let s = e.target.value;
+  const inputHandler = (e) => {
+    let search = e.target.value;
 
-    setState(prevState => {
-      return { ...prevState, searchQ: s}
+    setSearchTerm(prevState => {
+      return { ...prevState, search: search }
     });
 
-    //console.log(state.searchQ);
+    //console.log(searchTerm.search);
   }
 
   return (
     <div className="App">
       <header>
-        <h1>Movie and Tv Database</h1>
+        <h1>Search for a Movie</h1>
       </header>
       <main>
-        <Searchbar input={input} userSearch={userSearch} />
+        <Searchbar inputHandler={inputHandler} userSearch={userSearch} />
+        <ResultingSearch searchResults={searchTerm.searchResults} popUpInfo={popUpInfo} />
+        {(typeof searchTerm.selectedResult.title != "undefined") ? <Info selectedResult={searchTerm.selectedResult} exitPopUp={exitPopUp} /> : false }
       </main>
     </div>
   );
