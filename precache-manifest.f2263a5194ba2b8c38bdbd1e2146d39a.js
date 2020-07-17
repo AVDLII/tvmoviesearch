371308c477ceea6b6addfaea174d659b54df233b
Agrielio V. De Lazo II <avdelazo221b@gmail.com>
2020-07-17 12:10:16 +0800

    Updates

---
+++ precache-manifest.f2263a5194ba2b8c38bdbd1e2146d39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29185479a314513f5479f1393303ea9",
+    "url": "/tmdb-movie-search/index.html"
+  },
+  {
+    "revision": "16e878cc8e341d6d3782",
+    "url": "/tmdb-movie-search/static/css/main.6cdf640b.chunk.css"
+  },
+  {
+    "revision": "80309d88a4715df05e1a",
+    "url": "/tmdb-movie-search/static/js/2.3bb99749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tmdb-movie-search/static/js/2.3bb99749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e878cc8e341d6d3782",
+    "url": "/tmdb-movie-search/static/js/main.febafd97.chunk.js"
+  },
+  {
+    "revision": "1aad81b03b3e4df08f0c",
+    "url": "/tmdb-movie-search/static/js/runtime-main.e9b15e67.js"
+  }
+]);
